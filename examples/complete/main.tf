243e70e53d80df49ec800aa709d82b5a27b66eb6
Bruno Renato de Sousa Rodrigues <brenatosr@gmail.com>
2024-04-03 15:44:35 +0100

    Initial commit

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,7 +1,10 @@
 module "demo_s3_batch" {
+
   source  = "codurance/terraform-aws-s3-batch-transformer/aws"
   version = "1.0.0"
 
-
-  buckey_name = "MY_BUCKET_NAME"
+  project                       = "team_demo"
+  buckey_name                   = "MY_BUCKET_NAME"
+  bucket_prefix                 = ""
+  lambda_invoke_max_concurrency = 50
 }
